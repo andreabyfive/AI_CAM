38e191316863ccf8a05e38cb74f0b0e9eef5ad56
andreabyfive <andreabyfive@gmail.com>
2020-03-09 00:42:44 +0900

    add ai function

--- ARDUINO_Serial/Serial/Serial.ino
+++ ARDUINO_Serial/Serial/Serial.ino
@@ -20,29 +20,38 @@ void setup() {
 char buffer[32];        // 씨리얼 입력 데이터 버퍼
 int length = 31;        // 씨리얼로 읽어올 최대 데이터
 
-void loop() {
-  // digitalWrite(LED8, HIGH);
-  // digitalWrite(LED9, HIGH);
-  if (mySerial.available() > 0) {   // 데이터가 있으면
+int cls;  // 
+
+int aiGetClass(void){
+  return cls;
+}
+
+int aiInteraceLoop(void){
+    if (mySerial.available() > 0) {   // 데이터가 있으면
     char data = mySerial.read();  // 씨리얼 포트로 들어온 데이터를 버퍼에 저장
     buffer[0] = data;
     buffer[1] = 0;                                    // 버퍼마지막데이터 끝을 0으로 만들기
     Serial.write(buffer);
+
+    cls = atoi(buffer); // change to int
+    if(cls > 9) cls=0;  // 0~9 0 is defalt
+
+ /*  
+ // example  
     if (strcmp(buffer, "1") == 0) {  
-       Serial.println("LED1");// 들어온 데이터가 1이면
-      digitalWrite(LED9, LOW);    // turn the LED off by making the voltage LOW
-      digitalWrite(LED8, HIGH);   // turn the LED on (HIGH is the voltage level)
-  //    delay(500);
-    } else if (strcmp(buffer, "2") == 0) {                   // 들어온 데이터가 2이면
+       Serial.println("LED1");                          // 들어온 데이터가 1이면
+ 
+    } else if (strcmp(buffer, "2") == 0) {              // 들어온 데이터가 2이면
        Serial.println("LED2");
-      digitalWrite(LED8, LOW);    // turn the LED off by making the voltage LOW
-      digitalWrite(LED9, HIGH);    // turn the LED off by making the voltage LOW
- //     delay(500);
+  
     } else {
-  //    digitalWrite(LED8, LOW);    // turn the LED off by making the voltage LOW
-  //    digitalWrite(LED9, LOW);    // turn the LED off by making the voltage LOW
-
+  
     }
+*/
+    }
+}
 
-  }
+void loop() {
+  aiInteraceLoop();
+   
 }
